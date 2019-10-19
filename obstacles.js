602ce77d685640772d56f62dcdb85e63dba41e5b
asa24-corder <55647588+asa24-corder@users.noreply.github.com>
2019-10-19 08:39:09 +0400

    Delete obstacles.js

--- obstacles.js
+++
@@ -1,8 +0,0 @@
-function obstacle() {
-context.clearRect(0, 0, canvas.width, canvas.height)  
-obstacles.forEach(o => {
-o.x -= 1
-context.fillRect(o.x, o.y, o.width, o.height);      
-})
-requestAnimationFrame(obstacle);  
-}
