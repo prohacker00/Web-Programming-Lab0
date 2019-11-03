4930464fecbecc23d53f30de1c0586909343f952
IsmailEB <ismailelbolock@gmail.com>
2019-11-03 13:29:18 +0400

    Refactoring of the repo - signup and login

--- Project/app.js
+++ Project/app.js
@@ -2,19 +2,28 @@
 const express = require('express');
 const app = express();
 const serv = require ('http').Server(app);
-app.use(express.static(__dirname));
+var clients = 0;
 
+app.use(express.static(__dirname));
 app.use(express.static('client'));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/client/index.html');
+    res.sendFile(__dirname + '/client/htmlpublic/index.html');
 });
 
+var portNumber = (2000);
 serv.listen(2000);
 console.log('server connection')
 
 // socket.io import
 var io = require('socket.io') (serv, {});
+
 io.sockets.on('connection', function(socket) {
     console.log('socket connection');
+    clients++;
+})
+
+io.sockets.on('disconnect', function(socket) {
+    console.log('socket disconnection');
+    clients--;
 }) 
