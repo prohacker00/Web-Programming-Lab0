ea400219b5d7dbf8ff8838cf1cb97669037bbf1a
KevDave <55647493+KevDave@users.noreply.github.com>
2019-11-10 10:54:50 +0400

    Add files via upload
    Database to HTML link

---
+++ ConnectDB.php
@@ -0,0 +1,21 @@
+<?php
+
+$username = filter_input(INPUT_POST, 'username');
+$password = filter_input(INPUT_POST, 'score');
+if(!empty($username)){
+    if(!empty($score)){
+        $host = "localhost";
+        $dbusername = "root";
+        $dbscore = "root";
+        $dbname = "Shadow_Rise";
+    }
+    else{
+        echo "Score should not be empty";
+        die();
+    }
+}
+else{
+    echo "Username should not be empty";
+    die();
+}
+?>
