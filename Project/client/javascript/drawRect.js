1d0e1db0d16dce95f3e5b6880e68abf70d70113f
IsmailEB <ismailelbolock@gmail.com>
2019-11-22 22:33:00 +0400

    bar of health changes color as health change

--- Project/client/javascript/drawRect.js
+++ Project/client/javascript/drawRect.js
@@ -16,13 +16,52 @@ class drawRect {
     }
 
     healthBarPol(ctx, health) {
-        ctx.fillStyle="#FF0000";
+
+        if (health == 5) {
+            ctx.fillStyle = "#00FF00"
+        }
+
+        if (health == 4) {
+            ctx.fillStyle = "#5EFF00"
+        }
+
+        if (health == 3) {
+            ctx.fillStyle = "#F7FF00"
+        }
+
+        if (health == 2) {
+            ctx.fillStyle = "#FFA200"
+        }
+
+        if (health == 1) {
+            ctx.fillStyle = "#FF0000"
+        }
+        
         ctx.fillRect(20, 20, health*80, 20);
         
         }
 
     healthBarCriminal(ctx, health) {
-        ctx.fillStyle="#FF0000";
+        
+        if (health == 5) {
+            ctx.fillStyle = "#00FF00"
+        }
+
+        if (health == 4) {
+            ctx.fillStyle = "#5EFF00"
+        }
+
+        if (health == 3) {
+            ctx.fillStyle = "#F7FF00"
+        }
+
+        if (health == 2) {
+            ctx.fillStyle = "#FFA200"
+        }
+
+        if (health == 1) {
+            ctx.fillStyle = "#FF0000"
+        }
         ctx.fillRect(730, 20, health*80, 20);
         
         }
