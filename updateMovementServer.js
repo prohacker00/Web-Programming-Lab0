a5016b7d48b551165c6b0b824bd6836836cf8cd5
Yousuf El Baesy <yousuf5550@gmail.com>
2019-11-24 22:53:27 +0400

    testing
    testing to see whether heroku runs...

---
+++ updateMovementServer.js
@@ -0,0 +1,33 @@
+// This moves the players accordingly 
+
+module.exports.update = function (movement, object, bullet) {
+    const cool = require('./cooldown.js')
+
+    const jump = require('./jump')
+    if (movement.rightPressed) {
+        object.x += object.xSpeed
+        bullet.leftOrRight = "right"
+    }
+
+    if (movement.leftPressed) {
+        object.x -= object.xSpeed;
+        bullet.leftOrRight = "left"
+
+    }
+
+    if (movement.upPressed) {
+        object.inAir = true;
+        object.floating = false
+        if (!object.isFreeFalling) {
+            jump.jump(object);
+        }
+    }
+
+    if (movement.downPressed) {
+        cool.cooldown(bullet , object);
+    } 
+
+    if(movement.spacePressed) {
+        cool.cooldown(bullet , object)
+    }
+}
