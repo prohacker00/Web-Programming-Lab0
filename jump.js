296cc2ea8465de3e55ec40f594fea69fdd3607ed
IsmailEB <ismailelbolock@gmail.com>
2019-11-25 11:40:31 +0400

    deletesssssss

--- jump.js
+++
@@ -1,7 +0,0 @@
-// This is the method used to make the player jump
-
-module.exports.jump =  function(object1) {
-    object1.inAir = false
-    object1.y -= object1.gravity;
-    object1.gravity -= object1.ySpeed
-}
